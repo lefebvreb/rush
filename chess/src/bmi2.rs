fb9af5dd74ce4a57ddad48c83628606c8aecd202
Benjamin Lefebvre <benji.lef76@gmail.com>
2021-02-12 21:42:58 +0100

    Refactoring III

--- chess/src/bmi2.rs
+++ chess/src/bmi2.rs
@@ -1,6 +1,6 @@
 // Precalculated table of left shifts, such that
 // SHIFTS[i] == 1u64 << i for 0 <= i < 64
-pub const SHIFTS: [u64; 64] = [
+pub(crate) const SHIFTS: [u64; 64] = [
     0x1, 0x2, 0x4, 0x8, 0x10, 0x20, 0x40, 0x80,
     0x100, 0x200, 0x400, 0x800, 0x1000, 0x2000, 0x4000, 0x8000,
     0x10000, 0x20000, 0x40000, 0x80000, 0x100000, 0x200000, 0x400000, 0x800000,
@@ -14,14 +14,14 @@ pub const SHIFTS: [u64; 64] = [
 // Perform a parallel bits extract (pext) using the intrinsic (fast)
 #[inline(always)]
 #[cfg(target_feature = "bmi2")]
-pub fn pext(a: u64, mask: u64) -> u64 {
+pub(crate) fn pext(a: u64, mask: u64) -> u64 {
     unsafe {std::arch::x86_64::_pext_u64(a, mask)}
 }
 
 // Perform a parallel bits extract (pext) without the intrinsic (slow)
 #[inline(always)]
 #[cfg(not(target_feature = "bmi2"))]
-pub fn pext(a: u64, mut mask: u64) -> u64 {
+pub(crate) fn pext(a: u64, mut mask: u64) -> u64 {
     let (mut i, mut res) = (0, 0);
 
     while mask != 0 {
@@ -39,14 +39,14 @@ pub fn pext(a: u64, mut mask: u64) -> u64 {
 // Perform a parallel bits deposit (pdep) using the intrinsic (fast)
 #[inline(always)]
 #[cfg(target_feature = "bmi2")]
-pub fn pdep(a: u64, mask: u64) -> u64 {
+pub(crate) fn pdep(a: u64, mask: u64) -> u64 {
     unsafe {std::arch::x86_64::_pdep_u64(a, mask)}
 }
 
 // Perform a parallel bits deposit (pdep) without the intrinsic (slow)
 #[inline(always)]
 #[cfg(not(target_feature = "bmi2"))]
-pub fn pdep(a: u64, mut mask: u64) -> u64 {
+pub(crate) fn pdep(a: u64, mut mask: u64) -> u64 {
     let (mut i, mut res) = (0, 0);
 
     while mask != 0 {
